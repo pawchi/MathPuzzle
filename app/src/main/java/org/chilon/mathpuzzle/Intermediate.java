8c3233dca478e359dd1472c0c786b8cc85c2151f
pawchi <pawchi@o2.pl>
2018-05-22 12:48:47 +0200

    beckup1

---
+++ app/src/main/java/org/chilon/mathpuzzle/Intermediate.java
@@ -0,0 +1,12 @@
+package org.chilon.mathpuzzle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Intermediate extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intermediate);
+    }
+}
--- app/src/main/java/org/chilon/mathpuzzle/MainActivity.java
+++ app/src/main/java/org/chilon/mathpuzzle/MainActivity.java
@@ -1,7 +1,10 @@
 package org.chilon.mathpuzzle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button beginner = (Button)findViewById(R.id.beginner);
+
+        beginner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Beginner.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
